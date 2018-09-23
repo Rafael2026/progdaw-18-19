eb3f08a0418a7c9d24fa13516812c32ea62efaee
Juan <profesorjim@gmail.com>
2018-09-23 21:12:46 +0200

    progdaw update

---
+++ Tema 1/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+  public static void main(String args[]) {
+	  System.out.printf("Hola mundo! Número: %f %d Mensaje del profesor\n",34.0,45);
+	  System.out.printf("Vamos a empezar\n");
+  }
+}
---
+++ Tema 2/UsoDeOperadoresAritmeticos.java
@@ -0,0 +1,17 @@
+public class UsoDeOperadoresAritmeticos {
+  public static void main(String[] args) {
+    int x;
+    x = 100;
+    int resultadoParcial;
+
+    
+    resultadoParcial = x + 5;
+    x++;
+    
+    //x = x +1;
+    
+    
+    System.out.println(x + " " + resultadoParcial + " " + (x - 5));
+    System.out.println((x * 5) + " " + (x / 5) + " " + (x % 5));
+  }
+}
