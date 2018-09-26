1c756ca5001281be4b412567b110b93a65c9eb93
Juan Antº Jimenez <profesorjim@gmail.com>
2018-09-26 15:10:15 +0200

    actualizacion

--- Tema 4/sentenciaIfAnidada3.java
+++ Tema 4/sentenciaIfAnidada3.java
@@ -1,14 +1,18 @@
 
-public class sentenciaIf {
+public class sentenciaIfAnidada3 {
   public static void main (String[] args) {
     String linea;
   
-    System.out.println("Le voy a decir si el nº introducido es par o impar.");
+    System.out.println("Le voy a decir si el nº introducido es cero, par o impar.");
     System.out.println("Introduzca el nº: ");
     
     linea = System.console().readLine();
     int numero = Integer.parseInt(linea);
     
+    if (numero == 0) {
+      System.out.println("El número que has introducido es un CERO.");
+    }
+     
     if ((numero % 2) == 0) {  // el número es par
       System.out.printf("El número %d es par.\n",numero);
     } else {
