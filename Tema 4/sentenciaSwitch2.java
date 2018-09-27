403418b175ba14d91cb55d78098ce4e5c43ad088
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2018-09-27 11:47:42 +0200

    actualizacion

--- Tema 4/sentenciaSwitch2.java
+++ Tema 4/sentenciaSwitch2.java
@@ -1,23 +1,26 @@
 public class sentenciaSwitch2 {
   public static void main(String[] args) {
     String diaIndicado;
+    String diaDeLaSemana;
     
     System.out.println("Indica el día de la semana, y te digo lo que tienes a 1ª hora:");
     
     diaIndicado = System.console().readLine();
     
+    
     switch (diaIndicado) {
       case "lunes":
-        System.out.println("Sistemas Informáticos");
+        diaDeLaSemana = "Sistemas Informáticos";
         break;
       case "martes":
-        System.out.println("Programación");
+        diaDeLaSemana = "Programación";
         break;
       case "miercoles":
-        System.out.println("Entornos de desarrollo");
+        diaDeLaSemana = "Entornos de desarrollo";
         break;
       default:
-        System.out.println("No tengo ni idea");
+        diaDeLaSemana = "No tengo ni idea";
     }
+    System.out.println(diaDeLaSemana);
   }
 }
