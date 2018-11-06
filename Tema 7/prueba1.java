69cebbc165bb45721459b6f23d2f122f7a5e2330
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2018-11-06 09:18:40 +0100

    actualizacion

---
+++ Tema 7/prueba1.java
@@ -0,0 +1,16 @@
+public class prueba1 {
+  public static void main(String[] args) {
+    int[] patata = new int[5];
+    
+    patata[0] = 10;
+    patata[2] = 7;
+    patata[4] = 8;
+    
+    for (int i=0; i<5; i++) {
+        System.out.println(patata[i]);
+      
+    }
+    
+  }
+
+}
---
+++ Tema 7/prueba2.java
@@ -0,0 +1,16 @@
+public class prueba2 {
+  public static void main(String[] args) {
+    char[] patata = new char[5];
+    
+    patata[0] = 'a';
+    patata[2] = 'b';
+    patata[4] = 'c';
+    
+    for (int i=0; i<5; i++) {
+        System.out.println(patata[i]+".");
+      
+    }
+    
+  }
+
+}
