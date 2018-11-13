b662454b17a34c1d59a78e9535d2fd78d93988fa
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2018-11-13 09:37:49 +0100

    actualizacion

---
+++ Tema 7/ejercicio11.java
@@ -0,0 +1,69 @@
+public class ejercicio11 {
+  public static void main (String[] args) {
+    int[] numeros = new int[10];
+    
+    for (int i=0; i<10; i++) {
+      System.out.print("Introduzca valor posición "+i+": ");
+      numeros[i] = Integer.parseInt(System.console().readLine());
+    }
+    
+    // presentar array por pantalla
+    System.out.println("");
+    System.out.print("Posición:  ");
+    for (int i=0; i<10; i++) {
+      System.out.printf("%4d",i);
+    }
+    System.out.println("");
+    System.out.print("Valor:     ");
+    for (int i=0; i<10; i++) {
+      System.out.printf("%4d",numeros[i]);
+    }
+    System.out.println("");
+    
+    // colocar los primos al ppo
+    
+    int[] numerosPrimos = new int[10];
+    int[] numerosNoPrimos = new int [10];
+    int numPrimos = 0;
+    int numNoPrimos = 0;
+    
+    for (int i=0; i<10; i++) {
+      boolean esPrimo = true;
+      for (int j=2; (j<=(numeros[i]/2)) && esPrimo; j++) {
+        if (numeros[i] % j == 0) { // no es primo
+          esPrimo = false;
+        }
+      }
+      if (esPrimo) {
+        numerosPrimos[numPrimos] = numeros[i];
+        numPrimos++;
+      }
+      else {
+        numerosNoPrimos[numNoPrimos] = numeros[i];
+        numNoPrimos++;
+      }
+    }
+    
+    //pasamos al vector original los primos
+    for (int i=0; i<numPrimos; i++) {
+      numeros[i] = numerosPrimos[i];
+    }
+    // pasamos, a continuacion, nos no primos al vector original
+    for (int i=0; i<numNoPrimos; i++) {
+      numeros[numPrimos+i] = numerosNoPrimos[i];
+    }
+    
+     // presentar array por pantalla
+    System.out.printf("\n\n");
+    System.out.print("Posición:  ");
+    for (int i=0; i<10; i++) {
+      System.out.printf("%4d",i);
+    }
+    System.out.println("");
+    System.out.print("Valor:     ");
+    for (int i=0; i<10; i++) {
+      System.out.printf("%4d",numeros[i]);
+    }
+    
+  }
+}
--- Tema 7/ejercicio15.java
+++ Tema 7/ejercicio15.java
@@ -1,39 +1,67 @@
 public class ejercicio15 {
   public static void main (String[] args) { 
     int tamanio = 10;
+    int comensalesPorMesa = 4;
     int[] mesas = new int[tamanio];
     
     // estado inicial aleatorio de ocupación de mesas
     for (int i=0; i<tamanio; i++) {
-      mesas[i] = (int)(Math.random()*5);
+      mesas[i] = (int)(Math.random()*(comensalesPorMesa+1));
     }
     
     int grupoEntrante;
     
     do {
+      // presentación del estado de ocupación
+      
+      System.out.print("Mesa nº:    ");
+      for (int i=0; i<tamanio; i++) {
+        System.out.printf("%3d",i+1);
+      }
+      System.out.println("");
+      System.out.print("Ocupación:  ");
+      for (int i=0; i<tamanio; i++) {
+        System.out.printf("%3d",mesas[i]);
+      }
+      System.out.printf("\n\n");
+      
       System.out.print("Introduzca el nº de personas del grupo: ");
       grupoEntrante = Integer.parseInt(System.console().readLine());
-      if (grupoEntrante != -1) {
-        if (grupoEntrante > 4) {
+      if (grupoEntrante > 0) {
+        if (grupoEntrante > comensalesPorMesa) {
           System.out.println("Lo siento. No admitimos grupos de "+ 
           grupoEntrante + 
-          ". Haga grupos de 4 personas como máximo e intente de nuevo"); 
+          ". Haga grupos de "+comensalesPorMesa+" personas como máximo e intente de nuevo"); 
         }
         else {
           // buscar mesa vacía
           boolean grupoSentado = false;
-          for (int i=0; i<tamanio; i++) {
+          for (int i=0; (i<tamanio) && (grupoSentado==false); i++) {
             if (mesas[i] == 0) { // encontrada mesa vacía
               mesas[i] = grupoEntrante;
               grupoSentado = true;
               System.out.println("Por favor, siéntense en la mesa nº "+(i+1));
-              // forzar la finalización del bucle
-              i = tamanio;
             }
           }
           
           // en caso de no estar sentados, buscar hueco
+          if (grupoSentado==false) {
+            for (int i=0; (i<tamanio) && (grupoSentado==false); i++) {
+              if ((mesas[i] + grupoEntrante) <= comensalesPorMesa) { // encontrado hueco
+                mesas[i] += grupoEntrante;
+                grupoSentado = true;
+                System.out.print("Tendrán que compartir mesa. ");
+                System.out.println("Por favor, siéntense en la mesa nº "+(i+1));
+              }
+            }
+          }
           
+          // si no los hemos podido sentar, es que no hay hueco para ellos
+          if (grupoSentado == false) {
+            System.out.println("Lo siento, en estos momentos no queda sitio.");
+          }
+          
+          System.out.printf("\n\n");
           
         }
       }
