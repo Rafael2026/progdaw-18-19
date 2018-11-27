221f5271c6fb7456537180eb440e6f0ec8468ce5
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2018-11-27 11:29:02 +0100

    actualizacion

---
+++ Tema 8/pruebaCodigoAscii.java
@@ -0,0 +1,10 @@
+public class pruebaCodigoAscii {
+  public static void main (String[] args) {
+    int codigoAscii = 110; //la "n"
+    
+    System.out.println(codigoAscii);
+    System.out.println((char)codigoAscii);
+    
+    
+  }
+}
