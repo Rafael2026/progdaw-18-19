7b6523e68dd9a9ed6fc86a2fd6362a9be189b3c3
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2018-12-30 17:58:37 +0100

    actualizacion

--- Solucion Control 02 - parcial/libreria/numPerfectos.java
+++ Solucion Control 02 - parcial/libreria/numPerfectos.java
@@ -1,7 +1,7 @@
 package libreria;
 
 public class numPerfectos {
-  public static boolean divisorPropio (int candidato, int numero) {
+  public static boolean divisorPropio (long candidato, long numero) {
     boolean esDivisorPropio;
     if ((candidato != numero) && ((numero % candidato) == 0)) {
       esDivisorPropio = true;
@@ -11,9 +11,9 @@ public class numPerfectos {
     return esDivisorPropio;
   }
   
-  public static int sumaDivisoresPropios (int numero) {
-    int total = 0;
-    for (int i=1; i<=numero/2; i++) {
+  public static long sumaDivisoresPropios (long numero) {
+    long total = 0;
+    for (long i=1; i<=numero/2; i++) {
       if (divisorPropio(i,numero)) {
         total += i;
       }
@@ -21,7 +21,7 @@ public class numPerfectos {
     return total;
   }
   
-  public static boolean numeroPerfect (int numero) {
+  public static boolean numeroPerfect (long numero) {
     boolean esNumeroPerfecto;
     if (numero == sumaDivisoresPropios(numero)) {
       esNumeroPerfecto = true;
--- Solucion Control 02 - parcial/numerosCuriosos.java
+++ Solucion Control 02 - parcial/numerosCuriosos.java
@@ -1,20 +1,19 @@
-import libreria.numPerfectos;
+import libreria.numCuriosos;
 
-public class numerosPerfectos{
+public class numerosCuriosos {
   public static void main (String[] args) {
-    long valorUsuario;
+    int valorUsuario;
     
-    System.out.println("Este programa mostrará los números perfectos que hay entre 2");
+    System.out.println("Este programa mostrará los números curiosos que hay entre 2");
     System.out.println("y el valor que introduzca.");
     do {
       System.out.print("Introduzca un valor entero superior a 2: ");
       valorUsuario = Integer.parseInt(System.console().readLine());
     } while (valorUsuario <2);
-    for (long i=2; i<=valorUsuario; i++) {
-      if (numPerfectos.numeroPerfect(i)) {
+    for (int i=2; i<=valorUsuario; i++) {
+      if (numCuriosos.esNumCurioso(i)) {
         System.out.println (i);
       }
     }
-    
   }
 }
