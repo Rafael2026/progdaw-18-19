3273fc4240eab293e95c81ee910f8f302a8c5585
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2019-01-09 13:09:10 +0100

    actualizacion

---
+++ Tema 9/funcionamiento_this/usoThis.java
@@ -0,0 +1,28 @@
+public class usoThis {
+  
+  public static void main (String[] args) {
+    Caballo rocinante;
+    Caballo clavilenio;
+    
+    rocinante = new Caballo("Rocinante","macho");
+    clavilenio = new Caballo("Clavileño","macho");
+  
+    rocinante.setEdad(3);
+    rocinante.setPeso(350);
+    rocinante.setRaza("Español");
+    
+
+    System.out.println(rocinante.getNombre());
+  
+    System.out.println(rocinante.getEdad());
+    System.out.println();
+    
+    System.out.print(rocinante);
+    System.out.println();
+    System.out.print(clavilenio);
+    
+    System.out.println();
+    
+    
+  }
+}
